ea7807275d8e426a713f4b6045767f4349d11541
Cristian Ruiz <cristianrb235@gmail.com>
2022-12-08 23:28:36 +0100

    Add logger from zap

--- go.mod
+++ go.mod
@@ -3,15 +3,17 @@ module receipts-api
 go 1.18
 
 require (
-	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/mock v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/stretchr/testify v1.8.1
+	go.uber.org/zap v1.24.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/api/api.go
+++ internal/api/api.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"errors"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"receipts-api/internal/logger"
 	"receipts-api/internal/storage"
 	"receipts-api/internal/utils"
 	"receipts-api/pkg/types"
@@ -28,19 +31,24 @@ func (s *Server) Run() {
 	mux.HandleFunc("/receipts/{id}", s.GetReceiptById).Methods(http.MethodGet)
 	mux.HandleFunc("/receipts", s.GetAllReceipts).Methods(http.MethodGet)
 
+	logger.Info(fmt.Sprintf("Started server at port: %s", s.listenAddr))
 	http.ListenAndServe(s.listenAddr, mux)
 }
 
 // AddReceipt adds a receipt in the database
 func (s *Server) AddReceipt(writer http.ResponseWriter, req *http.Request) {
-	receipt := types.Receipt{}
-	if err := utils.ReadJSON(writer, req, &receipt); err != nil {
-		utils.ErrorJSON(writer, err, http.StatusBadRequest)
+	receipt := &types.Receipt{}
+	if err := utils.ReadJSON(writer, req, receipt); err != nil || receipt.Items == nil {
+		logger.Error("Error when reading json at AddReceipt handler", err)
+		if err == nil && receipt.Items == nil {
+			utils.ErrorJSON(writer, errors.New("invalid json"), http.StatusBadRequest)
+		}
 		return
 	}
 
-	insertedReceipt, err := s.mysqlStorage.CreateReceipt(&receipt)
+	insertedReceipt, err := s.mysqlStorage.CreateReceipt(receipt)
 	if err != nil {
+		logger.Error("Error when calling CreateReceipt", err)
 		utils.ErrorJSON(writer, err, http.StatusInternalServerError)
 		return
 	}
@@ -52,12 +60,14 @@ func (s *Server) GetReceiptById(w http.ResponseWriter, req *http.Request) {
 	receiptIdStr := mux.Vars(req)["id"]
 	receiptId, err := strconv.Atoi(receiptIdStr)
 	if err != nil {
+		logger.Error("Error when converting receipt id to int", err)
 		utils.ErrorJSON(w, err, http.StatusBadRequest)
 		return
 	}
 
 	receipt, err := s.mysqlStorage.GetReceiptById(receiptId)
 	if err != nil {
+		logger.Error("Error when calling GetReceiptById", err)
 		utils.ErrorJSON(w, err, http.StatusInternalServerError)
 		return
 	}
@@ -68,6 +78,7 @@ func (s *Server) GetReceiptById(w http.ResponseWriter, req *http.Request) {
 func (s *Server) GetAllReceipts(w http.ResponseWriter, req *http.Request) {
 	receipt, err := s.mysqlStorage.GetAllReceipts()
 	if err != nil {
+		logger.Error("Error when calling GetAllReceipts", err)
 		utils.ErrorJSON(w, err, http.StatusInternalServerError)
 		return
 	}
